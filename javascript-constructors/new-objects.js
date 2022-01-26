89aef142f6f35c4b1436e0e280b5abf67d4366d2
Grace Park <haeeunee@gmail.com>
2022-01-26 03:09:21 +0000

    Completed javascript-constructors.

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,18 @@
+var object1 = new Object();
+console.log('Value of object1: ', object1);
+console.log('typeof object1:', typeof object1);
+var array1 = new Array();
+console.log('Value of array1: ', array1);
+console.log('typeof array1:', typeof array1);
+var function1 = new Function();
+console.log('Value of function1: ', function1);
+console.log('typeof function1:', typeof function1);
+var string1 = new String();
+console.log('Value of string1: ', string1);
+console.log('typeof string1:', typeof string1);
+var number1 = new Number();
+console.log('Value of number1: ', number1);
+console.log('typeof number1:', typeof number1);
+var boolean1 = new Boolean();
+console.log('Value of boolean1: ', boolean1);
+console.log('typeof boolean1:', typeof boolean1);
